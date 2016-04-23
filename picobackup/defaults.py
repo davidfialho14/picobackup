e59dd90f499e63dfcd1f94ecf70ff656004bdb9f
David Fialho <fialho.david@gmail.com>
2016-04-23 23:51:25 +0100

    added creation of the app directories if they do not exist

--- picobackup/defaults.py
+++ picobackup/defaults.py
@@ -1,5 +1,8 @@
 import datetime
+import os
 import os.path as path
+
+from picobackup.utils import ignored
 
 home_dir = path.expanduser('~')
 app_dir = path.join(home_dir, '.picobackup')
@@ -8,3 +11,8 @@ log_file = path.join(app_dir, str(datetime.datetime.now())) + '.log'
 config_dir = path.join(app_dir, 'config')
 push_config_file = path.join(config_dir, 'push.conf')
 pull_config_file = path.join(config_dir, 'pull.conf')
+
+# ensure all necessary directories exist
+with ignored(OSError):
+    os.mkdir(app_dir)
+    os.mkdir(config_dir)
