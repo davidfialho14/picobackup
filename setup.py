389a30f5dfae36eed73eaac777b54c25bb62958b
David Fialho <fialho.david@gmail.com>
2016-04-23 16:21:59 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='picobackup',
+    version='1.0',
+    packages=find_packages(),
+    package_dir={'': 'pusher'},
+    url='',
+    license='MIT License',
+    author='David Fialho',
+    author_email='fialho.david@gmail.com',
+    description='',
+
+    install_requires=[],
+)
